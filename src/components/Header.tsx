ea0e6616ae080a59c7483961b121c466491f0358
lukeskw <lucasporfirioa@gmail.com>
2024-02-04 20:22:06 -0300

    refactor: changing header to get redux state

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,21 @@
+import { useAppSelector } from '../store'
+
 export function Header() {
+  const { currentModule, currentLesson } = useAppSelector((state) => {
+    const { currentModuleIdx, currentLessonIdx } = state.player
+
+    const currentModule = state.player.course.modules[currentModuleIdx]
+
+    const currentLesson = currentModule.lessons[currentLessonIdx]
+
+    return { currentModule, currentLesson }
+  })
   return (
     <div className="flex flex-col gap-1">
-      <h1 className="text-2xl font-bold">Redux Fundamentals</h1>
-      <span className="text-sm text-zinc-400">Module: Unveiling Redux</span>
+      <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
+      <span className="text-sm text-zinc-400">
+        Module: {currentModule.title}
+      </span>
     </div>
   )
 }
