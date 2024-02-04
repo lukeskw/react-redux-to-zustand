cf258188fe957971e4952bcf2519edf8300f5e56
lukeskw <lucasporfirioa@gmail.com>
2024-02-04 14:02:36 -0300

    initial commit with redux boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Provider as ReduxProvider } from "react-redux"
+
+import { store } from "./store"
+
+export function App() {
+
+  return (
+    <ReduxProvider store={store}>
+
+    </ReduxProvider>
+  )
+}
+
+export default App
