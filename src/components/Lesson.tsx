d421f5ca7a437c4688b97d0a6543b30fc8eed9db
lukeskw <lucasporfirioa@gmail.com>
2024-02-04 20:23:27 -0300

    refactor: changing Lesson to get redux state

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,15 +1,30 @@
-import { Video } from 'lucide-react'
+import { PlayCircle, Video } from 'lucide-react'
 
 interface ILessonProps {
   title: string
   duration: string
+  isCurrent?: boolean
+  onPlay: () => void
 }
 
-export function Lesson({ title, duration }: ILessonProps) {
+export function Lesson({
+  title,
+  duration,
+  onPlay,
+  isCurrent = false,
+}: ILessonProps) {
   return (
-    <button className="flex items-center gap-3 text-sm text-zinc-400">
-      <Video className="h-4 w-4 text-zinc-500" />
-      <span>{title}</span>
+    <button
+      onClick={onPlay}
+      data-playing={isCurrent}
+      className="flex items-center gap-3 text-sm text-zinc-400 *:hover:text-zinc-200 data-[playing=true]:text-emerald-600 *:data-[playing=true]:hover:text-emerald-500"
+    >
+      {isCurrent ? (
+        <PlayCircle className=" h-4 w-4 text-emerald-600" />
+      ) : (
+        <Video className=" h-4 w-4 text-zinc-500" />
+      )}
+      <span className="">{title}</span>
       <span className="ml-auto font-mono text-xs text-zinc-500">
         {duration}
       </span>
